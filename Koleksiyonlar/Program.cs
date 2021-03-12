8a39ca692cff809e01f6de7b4db587885ae2ed03
caferyavuz <caferyavuz134@gmail.com>
2021-03-12 21:08:13 +0300

    12 Mart Ders Sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+
+            Console.WriteLine("----------------------------------");
+
+            List<string> isimler2 = new List<string> {"Engin","Murat" };
+            isimler2.Add("Mehmet");
+
+            foreach (var item in isimler2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName+" sisteme eklendi.");
+            Console.WriteLine("-------------------------------------");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " gücellendi.");
+            Console.WriteLine("-------------------------------------");
+        }
+    }
+}
