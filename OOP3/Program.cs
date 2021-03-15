04d987ca9109e798384ae14fc3394c7181e0f3e5
caferyavuz <caferyavuz134@gmail.com>
2021-03-15 18:43:05 +0300

    15 Mart

--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -10,12 +10,14 @@ namespace OOP3
             IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
             IKrediManager tasitKrediManager = new TasitKrediManager();
             IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKrediManager();
 
             ILoggerService databaseLoggerService = new DatabaseLoggerService();
             ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerServices = new SmsLoggerService();
 
             BasvuruManager basvuruManager = new BasvuruManager();
-            basvuruManager.BasvuruYap(tasitKrediManager,databaseLoggerService);
+            basvuruManager.BasvuruYap(esnafKrediManager,smsLoggerServices);
             //basvuruManager.BasvuruYap(tasitKrediManager,new DatabaseLoggerService());
 
             List<IKrediManager> krediler = new List<IKrediManager>(){tasitKrediManager,konutKrediManager};
